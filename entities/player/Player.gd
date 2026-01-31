d3c4dde712787bb345cfe199430989b6a8395176
Fred <dinoqc@gmail.com>
2026-01-31 09:46:39 -0500

    transformation + mana costs + fix

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -9,15 +9,24 @@ const DASH_DURATION = 0.1
 
 var is_dashing = false
 var dash_timer = 0.0
+var currentMana = 100
+var is_hiding = false
+
+
+@export var maxMana = 100
+@export var manaRegen = 0.1
+@export var dashCost = 10
+@export var hideCost = 20
 
 func _physics_process(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if Input.is_action_just_pressed("dash") and direction and not is_dashing:
+	if Input.is_action_just_pressed("dash") and direction and not is_dashing and currentMana >= dashCost:
 		is_dashing = true
 		dash_timer = DASH_DURATION
 		velocity.x = direction.x * DASH_SPEED
 		velocity.z = direction.z * DASH_SPEED
+		currentMana -= dashCost;
 
 	if is_dashing:
 		dash_timer -= delta
@@ -28,9 +37,32 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION * delta)
 		
 		var target_angle = atan2(direction.x, direction.z)
-		$MeshInstance3D.rotation.y = lerp_angle($MeshInstance3D.rotation.y, target_angle, ROTATION_SPEED * delta)
+		$WitchModel.rotation.y = lerp_angle($WitchModel.rotation.y, target_angle, ROTATION_SPEED * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 		velocity.z = move_toward(velocity.z, 0, FRICTION * delta)
 
-	move_and_slide()
+	if !is_hiding:
+		move_and_slide()
+
+
+	if Input.is_action_just_pressed("hide") and not is_dashing:
+		becomeBox()
+
+	if !is_dashing and not is_hiding and currentMana < 100:
+		currentMana += manaRegen
+
+
+
+func becomeBox():
+
+	if (!is_hiding and currentMana >= hideCost):
+		is_hiding = true;
+		$WitchModel.hide()
+		$BoxModel.show()
+		currentMana -= hideCost;
+	else:
+		is_hiding = false
+		$WitchModel.show()
+		$BoxModel.hide()
+	
